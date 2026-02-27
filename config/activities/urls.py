6ff6a2aa787ea5745a89b396c4bf448bc3bae40b
TheOCJorsh <ogheghajoshua@gmail.com>
2026-02-27 16:38:08 +0100

    Update users activities with date range filter

--- config/activities/urls.py
+++ config/activities/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import ActivityListCreateView, ActivityDetailView, activity_list
 from .views import MilestoneListView
-
+from .views import ActivityAnalyticsView
 
 urlpatterns = [
     path('activities/', ActivityListCreateView.as_view(), name='activity-list'),
     path('activities/<int:pk>/', ActivityDetailView.as_view(), name='activity-detail'),
     path('activities/', activity_list),
     path('milestones/', MilestoneListView.as_view(), name='milestone-list'),
+    path('activities/analytics/', ActivityAnalyticsView.as_view(), name='activity-analytics'),
 ]
--- config/activities/views.py
+++ config/activities/views.py
@@ -5,7 +5,10 @@ from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
 from .models import Activity, Milestone
 from .serializers import ActivitySerializer, MilestoneSerializer
-
+from django.db.models import Sum, Count
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from datetime import datetime
 
 class ActivityListCreateView(generics.ListCreateAPIView):
     serializer_class = ActivitySerializer
@@ -34,6 +37,52 @@ class MilestoneListView(generics.ListAPIView):
         return Milestone.objects.filter(user=self.request.user)
     
 
+class ActivityAnalyticsView(APIView):
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+
+        start_date = request.query_params.get('start_date')
+        end_date = request.query_params.get('end_date')
+
+        activities = Activity.objects.filter(user=request.user)
+
+        if start_date and end_date:
+
+            try:
+                start_date = datetime.strptime(start_date, "%Y-%m-%d").date()
+                end_date = datetime.strptime(end_date, "%Y-%m-%d").date()
+
+                activities = activities.filter(
+                    activity_date__range=(start_date, end_date)
+                )
+
+            except ValueError:
+                return Response(
+                    {"error": "Invalid date format. Use YYYY-MM-DD"},
+                    status=400
+                )
+
+        analytics = activities.aggregate(
+
+            total_activities=Count('id'),
+
+            total_distance=Sum('distance_km'),
+
+            total_calories=Sum('calories_burned'),
+
+            total_duration=Sum('duration_minutes')
+
+        )
+
+        # Replace None with 0
+        analytics = {
+            key: value or 0
+            for key, value in analytics.items()
+        }
+
+        return Response(analytics)
 
 
 
